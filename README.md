032408996e164103411fe745022506f287e75e81
tahlilma <admimterminal2000@gmail.com>
2024-06-02 20:37:58 +0600

    finished writing the widget script

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 A small scribtable app which adds a widget to my iPhone homescreen and shows any new updates on that widget.
 The backends written in python and the widget part is written with scritable (js). 
 
-*If you want to run it figure it out yourself.*
+*If you want to run it figure it out yourself. Its really not hard, add your manga ids to the array in app.py and host then run script.js on scriptable*
--- script.js
+++ script.js
@@ -1,5 +1,32 @@
-function main() {
-  console.log('rofl')
-}
+const repeater = new Timer();
+repeater.repeats = true;
+repeater.timeInterval = 3600000;
 
-main()
+repeater.schedule(async () => {
+
+  let req = new Request(
+    "https://chaptminder-tahlil-0a4a4164.koyeb.app/get_updates"
+  );
+
+  let data = await req.loadJSON();
+  console.log(data);
+
+  const widget = new ListWidget();
+
+  widget.addDate(new Date());
+
+  let gradient = new LinearGradient();
+  gradient.locations = [0, 1];
+  gradient.colors = [new Color("4286F4"), new Color("373B44")];
+
+  widget.backgroundGradient = gradient;
+
+  let title = widget.addText("New Chapters:");
+  title.font = Font.lightRoundedSystemFont(30);
+
+  let count = widget.addText(`${data.count}`);
+  count.font = Font.heavyMonospacedSystemFont(60);
+
+  Script.setWidget(widget);
+
+});
